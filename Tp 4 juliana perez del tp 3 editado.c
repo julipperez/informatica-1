e1eb9c101ce9dc3ffe0cca4aa3e56bf56bb75faa
julipperez <123680075+julipperez@users.noreply.github.com>
2025-06-08 23:58:34 -0300

    Add files via upload

---
+++ Tp 4 juliana perez del tp 3 editado.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int nota;
+	
+	printf("ingrese la nota: ");
+	scanf("%d",&nota);
+	
+	if(nota>=90){
+		printf("calificacion A\n");
+			printf("Condicion excelente");
+	}else if (nota>=80 && nota<=90){
+		printf("calificacion B\n");
+			printf("condicion muy buena");
+	}else if (nota >=70 && nota<=80){
+		printf("calificacion C\n");
+			printf("Condicion buena");
+	}else if (nota>= 60 &&nota<=70){
+		printf("calificacion D\n");
+			printf("Condicion regular");
+	}else if (nota<60){
+		printf("calificacion F\n");
+			printf("Condicion insuficiente");
+	}
+	return 0;
+}
+
---
+++ tp 4_1R1_Juliana Perez.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int nota;
+	
+	printf("ingrese la nota: ");
+	scanf("%d",&nota);
+	
+	if(nota>=90){
+		printf("calificacion A");
+	}else if (nota>=80 && nota<=90){
+		printf("calificacion B");
+	}else if (nota >=70 && nota<=80){
+		printf("calificacion c");
+	}else if (nota>= 60 &&nota<=70){
+		printf("calificacion d");
+	}else if (nota<60){
+		printf("calificacion F");
+	}
+	return 0;
+}
+
