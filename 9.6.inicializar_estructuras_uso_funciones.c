3f07de8feed29ad66a7932de24ef2f6392822d64
julipperez <123680075+julipperez@users.noreply.github.com>
2025-11-05 17:49:52 -0300

    Add files via upload

---
+++ 9.6.inicializar_estructuras_uso_funciones.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+struct inventario{
+	char nombre[30];
+	int numero_parte;
+	float precio;
+	int stock;
+};
+void imprimir_inventario ( struct inventario item );
+int main(int argc, char *argv[]) {
+
+	struct inventario item1; // tengo una variable "item1"de tipo struct inventario
+	
+	strcpy(item1.nombre,"tomate");
+	item1.numero_parte=123;
+	item1.precio=32.5;
+	item1.stock=234;	
+	
+	imprimir_inventario(item1);
+	
+	return 0;
+}
+void imprimir_inventario ( struct inventario item ){
+	printf("el nombre es %s\n", item.nombre);
+	printf("el numero es: %d\n", item.numero_parte);
+	printf("el precio es: %f\n", item.precio);
+	printf("el numero de stock es: %d", item.stock);
+}
