bf8079756f190656d0e49952eab55fc89f62e908
julipperez <123680075+julipperez@users.noreply.github.com>
2025-11-05 17:50:33 -0300

    Add files via upload

---
+++ 8.7.paso_arreglos_a_funciones.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define TAM 5
+
+void carga ( int a[], int n);
+
+int main(int argc, char *argv[]) {
+	int n;
+	int arreglo [TAM];
+	
+	printf ("Ingrese la cantidad de elementos : ");
+	scanf (" %d", &n);
+
+	carga(arreglo, n);
+	
+	return 0;
+}
+
+void carga ( int a[], int n){
+	
+	for (int i = 0; i < n; i ++)
+	{
+		printf (" Ingrese el elemento [ %d]: ", i);
+		scanf (" %d", &a[i]);
+	}
+	for (int i = 0; i < n; i ++){
+		printf (" %d\n", a[i]);
+	}
+}
