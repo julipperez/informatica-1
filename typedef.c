7dbfb3d93bd17b8b6ab0b0a0fc7088a4384fe8e5
julipperez <123680075+julipperez@users.noreply.github.com>
2025-11-19 18:14:06 -0300

    Add files via upload

---
+++ typedef.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Definimos un tipo de dato "Persona" usando typedef
+typedef struct {
+	char nombre[50];
+	int edad;
+} Persona;
+
+int main() {
+	// Creamos variables del nuevo tipo Persona
+	Persona p1 = {"Natalia", 22};
+	Persona p2 = {"Carlos", 30};
+	
+	// Mostramos los datos
+	printf("Nombre: %s, Edad: %d\n", p1.nombre, p1.edad);
+	printf("Nombre: %s, Edad: %d\n", p2.nombre, p2.edad);
+	
+	return 0;
+}
+
