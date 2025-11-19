d38a2c6d35c401fda36d92824d3d1a482e7cdc78
julipperez <123680075+julipperez@users.noreply.github.com>
2025-11-19 18:03:09 -0300

    Add files via upload

---
+++ puntero_estructuras.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Persona {
+	char nombre[20];
+	int edad;
+};
+
+int main() {
+	struct Persona p;
+	struct Persona *ptr = &p;
+	
+	printf("Ingrese nombre: ");
+	scanf("%s", ptr->nombre);
+	
+	printf("Ingrese edad: ");
+	scanf("%d", &ptr->edad);
+	
+	printf("\nNombre: %s", ptr->nombre);
+	printf("\nEdad: %d\n", ptr->edad);
+	
+	return 0;
+}
