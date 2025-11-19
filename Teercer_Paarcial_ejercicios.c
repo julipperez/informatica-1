f65ad8dbcb2696f78f1d33dcf7e4393b55795391
julipperez <123680075+julipperez@users.noreply.github.com>
2025-11-19 12:15:28 -0300

    Add files via upload

---
+++ Teercer_Paarcial_ejercicios.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct inventario{
+	int p1,p2,p3;
+	char arreglo[3];
+	int *p;
+};
+int main(int argc, char *argv[]) {
+	int x=5;
+	int *p;
+	printf("el valor de x es: %d\n", x);
+	p=&x;
+	printf("el valor de x es: %d\n", *p);
+	*p=10;
+	printf("el valor de x es: %d\n", *p);
+	printf("el valor de x es: %d\n", x);
+	
+	//puntero a funcion
+	float (*pf)(float, float);
+	//arreglo de punteros
+	float (*arreglo[5])(float, float);
+	
+	//reservar memoria
+	struct inventario *pt=malloc(10 * sizeof(struct inventario));
+
+	return 0;
+}
+
