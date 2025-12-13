57e19f6ef563fe4c42ae5cc908def8404e58c75a
julipperez <123680075+julipperez@users.noreply.github.com>
2025-12-12 22:38:59 -0300

    Add files via upload

---
+++ 9.16 .punteros-estructuras-funciones.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+struct producto{
+	char nombre[20];
+	float precio;
+	int stock;
+};
+
+void cargar_producto(struct producto *p);
+void imprimir_producto(struct producto p);
+void actualizar_stock(struct producto *p, int cantidad);
+
+int main(int argc, char *argv[]) {
+	struct producto productito;
+	struct producto *b;
+	b=&productito;
+	
+	cargar_producto(b);//paso la direccion de memoria del puntero
+	imprimir_producto(productito); //paso toda la structura
+	
+	int valor;
+	printf("ingrese la cantidad de modificacion del stock. \n");
+	scanf("%d",&valor);
+	
+	actualizar_stock(b, valor);
+	
+	printf("\nProducto actualizado:\n");
+	imprimir_producto(productito);
+	
+	return 0;
+}
+void cargar_producto(struct producto *p){
+	printf("ingrese el nombre: \n");
+	scanf("%s", p->nombre);          
+	printf("ingrese el precio: \n");
+	scanf("%f", &p->precio);   
+	printf("ingreese el stock: \n");
+	scanf("%d", &p->stock);   
+	}
+void imprimir_producto(struct producto p){
+	printf("el nombre es: %s\n", p.nombre);
+	printf("el precio es: %.2f\n", p.precio);
+	printf("el stock es: %d\n", p.stock);
+}
+void actualizar_stock(struct producto *p, int cantidad){
+	p->stock= (p->stock)+ cantidad;
+}
---
+++ 9.20.punteros_estructuras.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+typedef struct {
+	char apodo[20];
+	int rango;
+	float vida;
+	float escudo;
+	float sales;
+	struct {
+		char nombre [20];
+		char apellido [20];
+		int edad;
+	} info_personal;
+} personaje_t;
+
+int main(int argc, char *argv[]) {
+	
+	personaje_t personaje , * pPersonaje ;
+	pPersonaje = &personaje ;
+	
+	pPersonaje->vida=150.0;
+ 	pPersonaje->escudo=0;
+	pPersonaje->sales=0;
+	printf("la vida es: %.2f\n", pPersonaje->vida);
+	printf("el escudo es: %.2f\n", pPersonaje->escudo);
+	printf("las sales son: %.2f\n", pPersonaje->sales);
+	
+	printf("ingrese el apodo: \n");
+	scanf("%s", pPersonaje->apodo);
+	printf("el apodo es: %s\n", pPersonaje->apodo);
+	
+	printf("ingrese el nombre: \n");
+	scanf("%s", pPersonaje->info_personal.nombre);
+	printf("el nombre es: %s\n", pPersonaje->info_personal.nombre);
+	
+	printf("ingrese el apellido: \n");
+	scanf("%s", pPersonaje->info_personal.apellido);
+	printf("el apellido es: %s\n", pPersonaje->info_personal.apellido);
+	
+	printf("ingrese edad: \n");
+	scanf("%d", &pPersonaje->info_personal.edad);
+	
+	return 0;
+}
+
